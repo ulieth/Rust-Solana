a43ca1c573f6bdbacfa2bf88ddff7aaada096cdf
ulieth <uliana.eth@proton.me>
2025-03-15 15:43:36 +0300

    using traits with fuzzybuzzy

--- rust-small-exercices/fizz-buzz/src/main.rs
+++ rust-small-exercices/fizz-buzz/src/main.rs
@@ -1,19 +1,31 @@
-fn fizz_buzz() {
-  let mut counter = 0;
-  for x in 1..=301 {
-    match (x % 3, x % 5) {
-      (0, 0) => {
-        counter += 1;
-        println!("FizzBuzz");
-      },
-      (0, _) => println!("Fizz"),
-      (_, 0) => println!("Buzz"),
-      _ => println!("{}", x),
-    }
-  }
-  println!("FizzBuzz occurred {} times.", counter);
+pub trait Fizzy {
+  fn fizzy(&self) -> String;
 }
 
+impl Fizzy for i32 {
+  fn fizzy(&self) -> String {
+
+      match (self % 3, self % 5) {
+        (0, 0) => String::from("FizzBuzz"),
+        (0, _) => String::from("Fizz"),
+        (_, 0) => String::from("Buzz"),
+        _ => format!("{}", self),
+      }
+
+  }
+
+}
+
+
+
 fn main() {
-    fizz_buzz();
+    let mut counter = 0;
+    for x in 1..=301 {
+      let result = x.fizzy();
+      println!("{}", x.fizzy());
+      if result == "FizzBuzz" {
+        counter += 1;
+      }
+    }
+    println!("FizzBuzz occurred {} times.", counter);
 }
