7c1a622b861b6afcb3d1c22aac7d4e664e848160
ulieth <uliana.eth@proton.me>
2025-03-15 15:09:33 +0300

    fizz-buzz simple version

--- rust-small-exercices/fizz-buzz/src/main.rs
+++ rust-small-exercices/fizz-buzz/src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello, world!");
+fn fizz_buzz() {
+  for x in 0..301 {
+    if x % 3 == 0 {println!("fizz")}
+    if x % 5 == 0 {println!("buzz")}
+    if x % 3 == 0 && x % 5 == 0 {println!("fizz buzz")}
+  }
+
 }
+
+fn main() {
+    fizz_buzz();
+}
+
+
+//At the end print the number of times "fizz buzz" occurred.
