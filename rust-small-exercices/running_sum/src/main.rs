d728b645175ba37d544abda92c3081d96ebd489a
ulieth <uliana.eth@proton.me>
2025-03-23 21:42:05 +0300

    rusty iterator

--- rust-small-exercices/running_sum/src/main.rs
+++ rust-small-exercices/running_sum/src/main.rs
@@ -4,14 +4,13 @@
 // See if you can figure out a "Rusty" way to do it using Rust's iterators and methods
 
 fn running_sum(nums: Vec<i32>) -> Vec<i32> {
-  let mut result = Vec::new();
-  let mut iterator = nums.iter();
-  let mut sum = 0;
-  while let Some(&num) = iterator.next() {
-      sum += num;
-      result.push(sum);
-  }
-  result
+
+ let cumsum: Vec<i32> = nums.into_iter().scan(0, |acc, x| {
+    *acc += x;
+    Some(*acc)
+ })
+ .collect();
+ cumsum
 }
 
 fn main() {
