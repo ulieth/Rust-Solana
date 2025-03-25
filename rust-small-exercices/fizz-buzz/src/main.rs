1137865dc9d43ffeb279dfdeb1c86cf2dc350da9
ulieth <uliana.eth@proton.me>
2025-03-25 09:52:05 +0300

    simple fizzbuzz

--- rust-small-exercices/fizz-buzz/src/main.rs
+++ rust-small-exercices/fizz-buzz/src/main.rs
@@ -1,31 +1,20 @@
-pub trait Fizzy {
-  fn fizzy(&self) -> String;
-}
-
-impl Fizzy for i32 {
-  fn fizzy(&self) -> String {
-
-      match (self % 3, self % 5) {
-        (0, 0) => String::from("FizzBuzz"),
-        (0, _) => String::from("Fizz"),
-        (_, 0) => String::from("Buzz"),
-        _ => format!("{}", self),
-      }
-
-  }
-
-}
-
-
 
 fn main() {
-    let mut counter = 0;
-    for x in 1..=301 {
-      let result = x.fizzy();
-      println!("{}", x.fizzy());
-      if result == "FizzBuzz" {
+  println!("{}","Welcome");
+  let mut counter: u32 = 0;
+  for ii in 1..=301 {
+    match (ii % 3, ii % 5) {
+      (0, 0) => {
+        println!("FizzBuzz");
         counter += 1;
+
       }
+      (0, _) => println!("Fizz"),
+      (_, 0) => println!("Buzz"),
+      _ => {}
     }
-    println!("FizzBuzz occurred {} times.", counter);
+  }
+  println!("{}",counter);
+
+
 }
